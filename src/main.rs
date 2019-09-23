2efa33ff825cd43aee2534cf8722d0356be77642
Max Strübing <mxstrbng@gmail.com>
2019-09-23 18:37:05 +0200

    feat(user): Add user sceleton

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 mod job;
 mod secret;
+mod user;
 
 fn main() {
     let secret = secret::Secret::new("hello", "world");
     secret.show();
 
-    let mut secrets = Vec::new();
-    secrets.push(secret::Secret::new("hello", "world"));
+    let secret = secret::Secret::new("hello", "world");
+    let job = job::Job::new("0 * * * *", "echo $hello", 0, 1, true, vec![secret]);
+    let user = user::User::new("max@mustermann.de", "abcdefg1", vec![job]);
 
-    let job = job::Job::new("0 * * * *", "echo $hello", 0, 1, true, secrets);
-
-    println!("{:?}", job);
-    println!("{:?}", secret);
-
-    println!("Hello, world!");
+    print!("{:?}", user);
 }

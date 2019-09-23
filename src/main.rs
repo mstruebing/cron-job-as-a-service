396cbeecf57dda9053f39e9f1108ceb2be94a43b
Max Strübing <mxstrbng@gmail.com>
2019-09-23 12:39:42 +0200

    feat(secret): Add secret sceleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod secret;
+
 fn main() {
+    let secret = secret::Secret::new("hello", "world");
+    secret.show();
+
     println!("Hello, world!");
 }
---
+++ src/secret.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Secret {
+    // TODO: Add id
+    key: &'static str,
+    value: &'static str,
+}
+
+impl Secret {
+    pub fn new(key: &'static str, value: &'static str) -> Self {
+        Secret { key, value }
+    }
+
+    pub fn show(&self) {
+        println!("{}: {}", self.key, self.value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let secret = Secret::new("hello", "world");
+        assert_eq!(secret.key, "hello");
+        assert_eq!(secret.value, "world");
+    }
+}

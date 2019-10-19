21a5a8e85029ec9991af9115bfaeb8b3df6660fc
Max Strübing <mxstrbng@gmail.com>
2019-10-19 12:43:21 +0200

    Add graphql delete mutations

--- api/src/graphql_schema.rs
+++ api/src/graphql_schema.rs
@@ -25,22 +25,25 @@ pub fn create_schema() -> Schema {
 #[juniper::object]
 impl QueryRoot {
     fn users() -> Vec<User> {
-        use crate::schema::users::dsl::*;
+        use crate::schema::users::dsl::users;
         let connection = establish_connection();
+
         users
             .load::<User>(&connection)
             .expect("Error loading users")
     }
 
     fn jobs() -> Vec<Job> {
-        use crate::schema::jobs::dsl::*;
+        use crate::schema::jobs::dsl::jobs;
         let connection = establish_connection();
+
         jobs.load::<Job>(&connection).expect("Error loading jobs")
     }
 
     fn secrets() -> Vec<Secret> {
-        use crate::schema::secrets::dsl::*;
+        use crate::schema::secrets::dsl::secrets;
         let connection = establish_connection();
+
         secrets
             .load::<Secret>(&connection)
             .expect("Error loading secrets")
@@ -51,6 +54,7 @@ impl QueryRoot {
 impl MutationRoot {
     fn create_user(data: NewUser) -> User {
         let connection = establish_connection();
+
         diesel::insert_into(users::table)
             .values(&data)
             .get_result(&connection)
@@ -59,6 +63,7 @@ impl MutationRoot {
 
     fn create_job(data: NewJob) -> Job {
         let connection = establish_connection();
+
         diesel::insert_into(jobs::table)
             .values(&data)
             .get_result(&connection)
@@ -67,6 +72,7 @@ impl MutationRoot {
 
     fn create_secret(data: NewSecret) -> Secret {
         let connection = establish_connection();
+
         diesel::insert_into(secrets::table)
             .values(&data)
             .get_result(&connection)
@@ -74,8 +80,9 @@ impl MutationRoot {
     }
 
     fn update_user(id: i32, data: UpdatedUser) -> User {
-        use crate::schema::users::dsl::*;
+        use crate::schema::users::dsl::users;
         let connection = establish_connection();
+
         diesel::update(users.find(id))
             .set(&data)
             .get_result(&connection)
@@ -83,8 +90,9 @@ impl MutationRoot {
     }
 
     fn update_job(id: i32, data: UpdadedJob) -> Job {
-        use crate::schema::jobs::dsl::*;
+        use crate::schema::jobs::dsl::jobs;
         let connection = establish_connection();
+
         diesel::update(jobs.find(id))
             .set(&data)
             .get_result(&connection)
@@ -92,12 +100,46 @@ impl MutationRoot {
     }
 
     fn update_secret(id: i32, data: UpdatedSecret) -> Secret {
-        use crate::schema::secrets::dsl::*;
+        use crate::schema::secrets::dsl::secrets;
         let connection = establish_connection();
+
         diesel::update(secrets.find(id))
             .set(&data)
             .get_result(&connection)
             .expect("Error updating secret")
+    }
+
+    fn delete_user(id: i32) -> bool {
+        use crate::schema::users::dsl::users;
+        let connection = establish_connection();
+
+        let num_deleted = diesel::delete(users.find(id))
+            .execute(&connection)
+            .expect("Error deleting user");
+
+        num_deleted != 0
+    }
+
+    fn delete_job(id: i32) -> bool {
+        use crate::schema::jobs::dsl::jobs;
+        let connection = establish_connection();
+
+        let num_deleted = diesel::delete(jobs.find(id))
+            .execute(&connection)
+            .expect("Error deleting job");
+
+        num_deleted != 0
+    }
+
+    fn delete_secret(id: i32) -> bool {
+        use crate::schema::secrets::dsl::secrets;
+        let connection = establish_connection();
+
+        let num_deleted = diesel::delete(secrets.find(id))
+            .execute(&connection)
+            .expect("Error deleting secret");
+
+        num_deleted != 0
     }
 }
 
@@ -137,8 +179,9 @@ impl User {
     }
 
     pub fn jobs(&self) -> Vec<Job> {
-        use crate::schema::jobs::dsl::*;
+        use crate::schema::jobs::dsl::{jobs, user_id};
         let connection = establish_connection();
+
         jobs.filter(user_id.eq(self.id))
             .load::<Job>(&connection)
             .expect("Error loading jobs for user")
@@ -199,8 +242,9 @@ impl Job {
     }
 
     pub fn secrets(&self) -> Vec<Secret> {
-        use crate::schema::secrets::dsl::*;
+        use crate::schema::secrets::dsl::{job_id, secrets};
         let connection = establish_connection();
+
         secrets
             .filter(job_id.eq(self.id))
             .load::<Secret>(&connection)
@@ -253,6 +297,7 @@ impl Secret {
 fn establish_connection() -> PgConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connection to {}", database_url))
 }
